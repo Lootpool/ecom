34686143c71687be0212afffe243ed3cc951e7a0
OkHelo-blip <62339876+Lootpool@users.noreply.github.com>
2025-04-14 22:41:40 +0530

    Update api_gateway.py

--- api_gateway.py
+++ api_gateway.py
@@ -4,9 +4,9 @@ import requests
 
 app=Flask(__name__)
 
-AUTH_SERVICE="http://localhost:5001"
-ORDER_SERVICE="http://localhost:5002"
-PRODUCT_SERVICE="http://localhost:5003"
+AUTH_SERVICE="https://auth-ecom-vzij.onrender.com"
+ORDER_SERVICE="https://order-service-ecom.onrender.com"
+PRODUCT_SERVICE="https://product-service-ecom.onrender.com"
 
 @app.route("/",methods=["GET"])
 def home():
