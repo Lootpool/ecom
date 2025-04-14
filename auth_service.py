e1ebb4e7bb99084a697088b2aeb53a0018e58816
OkHelo-blip <62339876+Lootpool@users.noreply.github.com>
2025-04-14 22:31:31 +0530

    Update auth_service.py

--- auth_service.py
+++ auth_service.py
@@ -41,6 +41,6 @@ def signup():
         return jsonify({"success":False,"msg":"AUTH SERVICE"+str(e)}),200
 
 def main():
-    app.run(debug=True,host="0.0.0.0",port=10000)
+    app.run(debug=False,host="0.0.0.0",port=10000)
 if __name__=='__main__':
     main()
